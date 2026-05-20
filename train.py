975857ae62525039fbc27a936b3acdcc3df4ff47
dwrrio <107758775+dwrrio@users.noreply.github.com>
2026-05-20 17:03:33 +0330

    Create train.py

---
+++ train.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+
+from env.trading_env import TradingEnv
+from agents.dqn import DQNAgent
+
+
+df = pd.read_csv("data/apple.csv")
+
+
+env = TradingEnv(df)
+state_size = env.observation_space.shape[0]
+action_size = env.action_space.n
+
+
+agent = DQNAgent(
+    state_size,
+    action_size
+)
+
+
+episodes = 50
+
+for episode in range(episodes):
+
+    state, info = env.reset()
+    done = False
+    total_reward = 0
+
+    while not done:
+
+        action = agent.act(state)
+        next_state, reward, done, truncated, info = env.step(action)
+        agent.remember(
+            state,
+            action,
+            reward,
+            next_state,
+            done
+        )
+
+        agent.replay()
+        state = next_state
+        total_reward += reward
+
+    print(f"Episode: {episode + 1}")
+    print(f"Reward: {total_reward:.2f}")
+    print(f"Portfolio Value: {info['portfolio_value']:.2f}")
+    print(f"Epsilon: {agent.epsilon:.4f}")
